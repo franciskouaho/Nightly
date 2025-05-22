b93b6b77e790ff4be26c3a2296ff85e142a49a47
Francis Kouaho <f.kouaho@skipperndt.com>
2025-05-22 14:48:49 +0200

    feat(front): go review apple
    Signed-off-by: Francis Kouaho <f.kouaho@skipperndt.com>

--- app/data/gameModes.ts
+++ app/data/gameModes.ts
@@ -1,126 +1,126 @@
 // Définition des interfaces
 export interface GameMode {
-  id: string;
-  name: string;
-  description: string;
-  image: any;
-  colors: string[];
-  borderColor: string;
-  shadowColor: string;
-  tag: string;
-  tagColor: string;
-  premium: boolean;
-  interactive?: 'write' | 'choice' | 'action';
+    id: string;
+    name: string;
+    description: string;
+    image: any;
+    colors: string[];
+    borderColor: string;
+    shadowColor: string;
+    tag: string;
+    tagColor: string;
+    premium: boolean;
+    interactive?: 'write' | 'choice' | 'action';
 }
 
 export interface GameCategory {
-  id: string;
-  title: string;
-  subtitle: string;
-  games: GameMode[];
+    id: string;
+    title: string;
+    subtitle: string;
+    games: GameMode[];
 }
 
 // Configuration des catégories de jeu
 export const gameCategories: GameCategory[] = [
-  {
-    id: 'nightly_modes',
-    title: 'SUGGESTION DE LA SEMAINE',
-    subtitle: '',
-    games: [
     {
-        id: 'trap-answer',
-        name: "TRAP ANSWER",
-        description: "Un quiz où une mauvaise réponse vous fait perdre des points... Saurez-vous l'éviter ?",
-        image: require('@/assets/jeux/trap-answer.png'),
-        colors: ["#1A2A5B", "#7B24B1"],
-        borderColor: "#2C2C2C",
-        shadowColor: "#2C2C2C",
-        tag: 'GRATUIT',
-        tagColor: "#8E24AA",
-        premium: false,
-        interactive: 'choice'
-      },
-      {
-        id: 'listen-but-don-t-judge',
-        name: 'ON ÉCOUTE MAIS ON NE JUGE PAS',
-        description: 'Un mode gratuit pour rigoler tranquillement entre potes.',
-        image: require('@/assets/jeux/on-ecoute-mais-on-juge-pas.png'),
-        colors: ["rgba(17, 34, 78, 0.8)", "rgba(38, 56, 120, 0.9)"],
-        borderColor: "#3B5FD9",
-        shadowColor: "#3B5FD9",
-        tag: 'GRATUIT',
-        tagColor: "#8E24AA",
-        premium: false,
-        interactive: 'write'
-      }
-    ]
-  },
-  {
-    id: 'same_room',
-    title: 'DANS LA MÊME PIÈCE',
-    subtitle: 'À jouer dans la même pièce, ensemble !',
-    games: [
-      {
-        id: 'truth-or-dare',
-        name: 'ACTION OU VÉRITÉ',
-        description: 'Le classique revisité avec des défis exclusifs.',
-        image: require('@/assets/jeux/action-verite.png'),
-        colors: ["rgba(50, 90, 150, 0.8)", "rgba(80, 120, 200, 0.9)"],
-        borderColor: "#3F51B5",
-        shadowColor: "#3F51B5",
-        tag: 'PREMIUM',
-        tagColor: '#D81B60',
-        premium: false,
-        interactive: 'action'
-      },
-      {
-        id: 'never-have-i-ever-hot',
-        name: "JE N'AI JAMAIS 🔞",
-        description: 'Questions coquines et déplacées... Prêts à assumer ?',
-        image: require('@/assets/jeux/hot.png'),
-        colors: ["rgba(90, 10, 50, 0.8)", "rgba(130, 20, 80, 0.9)"],
-        borderColor: "#D81B60",
-        shadowColor: "#D81B60",
-        tag: 'PREMIUM',
-        tagColor: "#D81B60",
-        premium: false,
-        interactive: 'write'
-      }
-    ]
-  },
-  {
-    id: 'online',
-    title: 'À DISTANCE',
-    subtitle: 'Pour jouer même quand on n\'est pas ensemble',
-    games: [
-      {
-        id: 'genius-or-liar',
-        name: 'GENIE OU MENTEUR',
-        description: 'Un mode ludique où vous devez prouver vos connaissances ou assumer vos gages.',
-        image: require('@/assets/jeux/genius-or-liar.png'),
-        colors: ["rgba(20, 20, 40, 0.8)", "rgba(40, 40, 80, 0.9)"],
-        borderColor: "#212121",
-        shadowColor: "#212121",
-        tag: 'PREMIUM',
-        tagColor: "#D81B60",
-        premium: false,
-        interactive: 'write'
-      },
-      {
-        id: 'the-hidden-village',
-        name: "LE VILLAGE CACHÉ",
-        description: 'Un jeu de bluff, de stratégie et de discussions... pour ceux qui aiment accuser leurs potes 😈',
-        image: require('@/assets/jeux/levillagecache.png'),
-        colors: ["rgba(147, 51, 234, 0.8)", "rgba(192, 38, 211, 0.9)"],
-        borderColor: "#A855F7",
-        shadowColor: "#A855F7",
-        tag: 'PREMIUM',
-        tagColor: "#D81B60",
-        premium: false,
-        interactive: 'choice'
-      },
-    ]
-  }
+        id: 'nightly_modes',
+        title: 'SUGGESTION DE LA SEMAINE',
+        subtitle: '',
+        games: [
+            {
+                id: 'trap-answer',
+                name: "TRAP ANSWER",
+                description: "Un quiz où une mauvaise réponse vous fait perdre des points... Saurez-vous l'éviter ?",
+                image: require('@/assets/jeux/trap-answer.png'),
+                colors: ["#1A2A5B", "#7B24B1"],
+                borderColor: "#2C2C2C",
+                shadowColor: "#2C2C2C",
+                tag: 'GRATUIT',
+                tagColor: "#8E24AA",
+                premium: false,
+                interactive: 'choice'
+            },
+            {
+                id: 'listen-but-don-t-judge',
+                name: 'ON ÉCOUTE MAIS ON NE JUGE PAS',
+                description: 'Un mode gratuit pour rigoler tranquillement entre potes.',
+                image: require('@/assets/jeux/on-ecoute-mais-on-juge-pas.png'),
+                colors: ["rgba(17, 34, 78, 0.8)", "rgba(38, 56, 120, 0.9)"],
+                borderColor: "#3B5FD9",
+                shadowColor: "#3B5FD9",
+                tag: 'PREMIUM',
+                tagColor: "#D81B60",
+                premium: true,
+                interactive: 'write'
+            }
+        ]
+    },
+    {
+        id: 'same_room',
+        title: 'DANS LA MÊME PIÈCE',
+        subtitle: 'À jouer dans la même pièce, ensemble !',
+        games: [
+            {
+                id: 'truth-or-dare',
+                name: 'ACTION OU VÉRITÉ',
+                description: 'Le classique revisité avec des défis exclusifs.',
+                image: require('@/assets/jeux/action-verite.png'),
+                colors: ["rgba(50, 90, 150, 0.8)", "rgba(80, 120, 200, 0.9)"],
+                borderColor: "#3F51B5",
+                shadowColor: "#3F51B5",
+                tag: 'PREMIUM',
+                tagColor: '#D81B60',
+                premium: true,
+                interactive: 'action'
+            },
+            {
+                id: 'never-have-i-ever-hot',
+                name: "JE N'AI JAMAIS 🔞",
+                description: 'Questions coquines et déplacées... Prêts à assumer ?',
+                image: require('@/assets/jeux/hot.png'),
+                colors: ["rgba(90, 10, 50, 0.8)", "rgba(130, 20, 80, 0.9)"],
+                borderColor: "#D81B60",
+                shadowColor: "#D81B60",
+                tag: 'PREMIUM',
+                tagColor: "#D81B60",
+                premium: true,
+                interactive: 'write'
+            }
+        ]
+    },
+    {
+        id: 'online',
+        title: 'À DISTANCE',
+        subtitle: 'Pour jouer même quand on n\'est pas ensemble',
+        games: [
+            {
+                id: 'genius-or-liar',
+                name: 'GENIE OU MENTEUR',
+                description: 'Un mode ludique où vous devez prouver vos connaissances ou assumer vos gages.',
+                image: require('@/assets/jeux/genius-or-liar.png'),
+                colors: ["rgba(20, 20, 40, 0.8)", "rgba(40, 40, 80, 0.9)"],
+                borderColor: "#212121",
+                shadowColor: "#212121",
+                tag: 'PREMIUM',
+                tagColor: "#D81B60",
+                premium: true,
+                interactive: 'write'
+            },
+            {
+                id: 'the-hidden-village',
+                name: "LE VILLAGE CACHÉ",
+                description: 'Un jeu de bluff, de stratégie et de discussions... pour ceux qui aiment accuser leurs potes 😈',
+                image: require('@/assets/jeux/levillagecache.png'),
+                colors: ["rgba(147, 51, 234, 0.8)", "rgba(192, 38, 211, 0.9)"],
+                borderColor: "#A855F7",
+                shadowColor: "#A855F7",
+                tag: 'PREMIUM',
+                tagColor: "#D81B60",
+                premium: true,
+                interactive: 'choice'
+            },
+        ]
+    }
 ];
 
 export default gameCategories;
